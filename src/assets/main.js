a8cef9433e7eab4b3d773101b68951c1585df6a1
Usman Shahid <usmanshahid@live.com>
2018-01-18 22:02:41 +0500

    Making Div
    Making div and adding classes using jQuery.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/UsmanShahid9585.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed);
+    }
+  });
 });
+
+function addCourses(courses) {
+  var $badges = $('#badges');
+  courses.forEach(function(course) {
+    var $course = $('<div />', {
+      'class': 'course'
+    }).appendTo($badges);
+  })
+}
