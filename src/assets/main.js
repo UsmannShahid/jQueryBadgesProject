5d195cc2dfe8d4ba7ece6beae794bd64e07e5d21
Usman Shahid <usmanshahid@live.com>
2018-01-19 10:14:53 +0500

    Final Submit
    End of project.

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,13 +7,36 @@ $(function() {
       addCourses(response.courses.completed);
     }
   });
-});
 
-function addCourses(courses) {
-  var $badges = $('#badges');
-  courses.forEach(function(course) {
-    var $course = $('<div />', {
-      'class': 'course'
-    }).appendTo($badges);
-  })
-}
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+
+
+      $('<a />', {
+        href: course.url,
+        target: '_blank',
+        text: 'See Course',
+        'class': 'btn btn-primary'
+      }).appendTo($course);
+
+    })
+
+
+  }
+});
